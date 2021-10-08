86a9236a663a4583cd1897e23dc1d6a9ef1f8029
ankit <ankit.sanghavi87@gmail.com>
2021-10-08 13:09:59 +0530

    fin

--- routes/me.js
+++ routes/me.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const checkAuth = require("../middleware/checkAuth");
 const User = require('../models/User')
+const bcrypt = require('bcrypt');
 
 router.put(`/bio`,checkAuth, async (req, res) => {
     const {
@@ -32,6 +33,30 @@ router.put(`/name`,checkAuth, async (req, res) => {
 
     const fieldToUpdate = {
         name:name
+    }
+
+    const user = await User.findOneAndUpdate(
+        { email: userEmailId },
+        { $set: { ...fieldToUpdate } },
+        {
+          runValidators: true,
+          new: true,
+        })
+    
+    await user.save()
+    return res.json(user);
+})
+
+router.put(`/password`,checkAuth, async (req, res) => {
+    const {
+        password,
+        userEmailId
+    } = req.body
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const fieldToUpdate = {
+        password:hashedPassword
     }
 
     const user = await User.findOneAndUpdate(
